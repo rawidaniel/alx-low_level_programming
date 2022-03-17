13ece05d4a0eb7521a453d4ea3a539e3c8c77431
Rawi Daniel <rawidaniel96@gmail.com>
2022-03-17 09:42:46 -0700

    Fix output error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,22 +15,22 @@ int main(void)
 	if (i % 5 == 0 && i % 3 == 0)
 	{
 		printf("FizzBuzz");
-		printf(" ");
+		putchar(32);
 	}
 	else if (i % 3 == 0)
 	{
 		printf("Fizz");
-		printf(" ");
+		putchar(32);
 	}
 	else if (i % 5 == 0)
 	{
 		printf("Buzz");
-		printf(" ");
+		putchar(32);
 	}
 	else
 	{
 		printf("%d", i);
-		printf(" ");
+		putchar(32);
 	}
 	}
 	printf("\n");
