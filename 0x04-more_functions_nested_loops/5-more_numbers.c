3c53feb45bc2fed2d6944952133a334108bf3910
Rawi Daniel <rawidaniel96@gmail.com>
2022-03-17 06:07:26 -0700

    Print list of numbers ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * more_numbers - print number from 0 t0 14 ten times
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -9,3 +9,5 @@
    * A function that prints the numbers, from 0 to 9, followed by a new line.
 5. 4-print_most_numbers.c
    * A function that prints the numbers, from 0 to 9, followed by a new line.
+6. 5-more_numbers.c
+   * A function that prints 10 times the numbers, from 0 to 14, followed by a new line.
