3d65837634ce7ac1f1af80d44e3f5b0c0f622154
Rawi Daniel <rawidaniel96@gmail.com>
2022-03-24 02:13:57 -0700

    Convert form lowercase to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * string_toupper - convert all lowercase letter to uppercase
+ * @s: array
+ * Return: string 
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		if (s[j] >= 'a' && s[j] <= 'z')
+		{
+			s[j] = s[j] -32;
+		}
+	}
+	return (s);
+}
