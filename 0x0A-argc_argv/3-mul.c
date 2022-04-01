0bcafebbdb11e0ad5c4a8ef536a4b4d08135b3d0
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-01 07:48:00 -0700

    Fix output error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,22 +4,21 @@
  * main - check the code
  *@argc: the number of command line arguments
  *@argv: an array containing the program command line argument
- * Return: Always 0.
+ * Return: 1 if it doesn't receive two argument otherwise 0.
  */
 int main(int argc, char *argv[])
 {
 	int i, multiply = 1;
 
-	if (argc != 1)
+	if (argc > 2)
 	{
 	for (i = 1; i < argc; i++)
 	{
 		multiply *= atoi(argv[i]);
 	}
 		printf("%d\n", multiply);
-	} else
-	{
-		printf("Error\n");
+		return (0);
 	}
-	return (0);
+	printf("Error\n");
+	return (1);
 }
