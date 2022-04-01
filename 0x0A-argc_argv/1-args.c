5050ff6c91bc3ec4e2ce652995f3de23369b3cf4
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-01 08:25:52 -0700

    prints the number of arguments passed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  *@argv: an array containing the program command line argument
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	int i;
 
