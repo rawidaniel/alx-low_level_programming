8da1dd19984e045d605f58440e505fc6bcdc2d8d
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-07 10:56:23 -0700

    Fix output error

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,7 +12,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 	void *new;
 
-	if (new_size == 0 && ptr == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
