a6f29ff34d224a161c488e101f783f5dfad62403
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-11 07:34:44 -0700

    Fix output error

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,10 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (d->age == 0)
-		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 	printf("Name: %s\n", d->name);
