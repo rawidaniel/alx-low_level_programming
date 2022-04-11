430d94d57f7b649c9e178cffb14e8b5abcdbb0b9
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-11 07:05:56 -0700

    Fix output error

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,13 +2,15 @@
 #include <stdlib.h>
 /**
  * init_dog - initialize a variable type struct dog
- * @d: a pointer
- * @name: a pointer
+ * @d: variable of a type struct dog
+ * @name: a dog name
  * @age: age of the dog
  * @owner: owner of the dog
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
