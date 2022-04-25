7b7cc261d6e424737dcef8c6531f4e35ff82b82d
Rawi Daniel <rawidaniel96@gmail.com>
2022-04-25 05:29:31 -0700

    add node at the end of the linked list

---
+++ 0x13-more_singly_linked_list/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - add a new node at the end of a listint_t list
+ * @head: pointer to head node
+ * @n: integer value assigned to element of struct listint_s
+ * Return: address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, *temp;
+
+	new = (listint_t *)malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	return (new);
+}
--- 0x13-more_singly_linked_list/README.md
+++ 0x13-more_singly_linked_list/README.md
@@ -3,3 +3,5 @@
    * a function that prints all the elements of a listint_t list where the prototype is size_t print_listint(const listint_t *h).
 2. 1-listint_len.c
    * A function that returns the number of elements in a linked listint_t list where the prototype is size_t listint_len(const listint_t *h).
+3. 2-add_nodeint.c
+   * A function that adds a new node at the beginning of a listint_t list where the prototype is listint_t *add_nodeint(listint_t **head, const int n) and returns the address of the new element, or NULL if it failed.
