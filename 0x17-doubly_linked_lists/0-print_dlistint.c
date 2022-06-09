96c7a82d9bd711fe757cda1512e6b914724d5a9a
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-09 13:26:43 -0700

    print all element of linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ * @h: pointer to first node
+ * Return: the number of node
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count = 0;
+	while (h)
+	{
+		printf("%d\n",h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
