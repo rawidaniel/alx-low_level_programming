ea29af0442d23796252f68a23a2d7886fe359d00
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-02 00:09:18 -0700

    fix bug

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index;
 
+	if (!ht || !key || strlen(key) == 0)
+		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
