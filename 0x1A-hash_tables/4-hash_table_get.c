37c163f6333f8306bdbc8b071480a23013c955a3
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-02 00:04:52 -0700

    retrieve the value

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: value associated with the key on success
+ * or NULL if key couldn't be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+	if (node == NULL)
+		return (NULL);
+	return (node->value);
+}
