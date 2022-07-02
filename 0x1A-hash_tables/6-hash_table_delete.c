17867fe4d4b4c8be855e47901f0a7babcf07ac53
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-02 02:01:39 -0700

    delete hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *tmp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+	if (!ht->array)
+	{
+		free(ht);
+		return;
+	}
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			tmp = node->next;
+			if (node->value)
+				free(node->value);
+			if (node->key)
+				free(node->key);
+			free(node);
+			node = tmp;
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
