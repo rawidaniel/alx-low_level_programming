980bc393a936060160b44afacf4d27f9d0e82778
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-05 05:34:04 -0700

    fix minor bug

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -28,6 +28,6 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = pos - 1;
 	}
-	printf("Value checked array[%ld] is out of range\n", high);
+	printf("Value checked array[%ld] is out of range\n", pos);
 	return (-1);
 }
